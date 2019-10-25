fc22462611ca7c759a24927ce0084a6576910917
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-25 10:14:10 -0600

    registration route complete

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/users");
+const bcrypt = require("bcryptjs")
 
-//need to think about if this file needs to be relational
-//ex: const name = require('../models/name.js');
-//need routes
 
+router.post('/registration', async (req, res) => {
+    const password = req.body.password;
+    const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    const newUser = {};
+    newUser.username = req.body.username;
+    newUser.password = passwordHash;
+    newUser.email = req.body.email;
+
+    const createdUser = await User.create(newUser);
+    console.log(`Created User ==>`,createdUser)
+    req.session.username = createdUser.username;
+    req.session.logged = true;
+
+    res.redirect('/groups')
+})
+ 
 
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
+ 
+const userSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String
+    },
+    email: String,
+    password: {
+        required: true,
+        type: String
+    }
+});
 
-//need schema
-//need model
-//need to export
+module.exports = mongoose.model('User', userSchema);
