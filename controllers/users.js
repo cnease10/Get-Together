03964354dd14de1e859656cb19e67401604d9a85
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-25 10:19:52 -0600

    users controller complete

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/users");
 const bcrypt = require("bcryptjs")
 
-
+//Registration post
 router.post('/registration', async (req, res) => {
     const password = req.body.password;
     const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
@@ -19,7 +19,40 @@ router.post('/registration', async (req, res) => {
 
     res.redirect('/groups')
 })
+
+//Login post
+router.post('/login', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({ username: req.body.username });
+        if (foundUser) {
+            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                req.session.message = '';
+                req.session.username = foundUser.username;
+                req.session.logged = true;
+                res.redirect('/groups')
+            } else {
+                req.session.message = 'Username/password incorrect';
+                res.redirect('/');
+            }
+        } else {
+            req.session.message = 'Username/password incorrect';
+            res.redirect('/');
+        }
+    } catch (err) {
+        res.send(err);
+    }
+});
  
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/');
+        }
+    })
+})
 
 
 module.exports = router;
